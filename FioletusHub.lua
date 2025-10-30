ac3f560d19b8ebacea64c80551705ad659c1a847
TikTokGG67439 <s3ventt@gmail.com>
2025-10-30 17:12:24 +0200

    FioletusHub.lua

--- FioletusHub.lua
+++ FioletusHub.lua
@@ -29,7 +29,7 @@ local function safeHttpGet(url)
 end
 
 local LOGIC_URL = "https://raw.githubusercontent.com/TikTokGG67439/FioletusHubScript.lua/refs/heads/main/FioletusLogic2.lua" -- << replace with your raw file URL
-local VISUALS_URL = "PASTE_RAW_URL_HERE/FioletusLogic2.txt" -- << replace with your raw file URL
+local VISUALS_URL = "https://raw.githubusercontent.com/TikTokGG67439/FioletusHubScript.lua/refs/heads/main/FioletusLogic2.lua" -- << replace with your raw file URL
 
 -- Fetch & execute Logic 1
 local ok, res = pcall(function()
